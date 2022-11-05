4c07f0fa07899d78bd5d30a8f3fd6234b7dfcfe2
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-05 16:52:22 -0400

    added function

--- main.cpp
+++ main.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 
 void sort2(double* p, double* q){
-
+	if (*p > *q) {
+		double* temp = p;
+		p = q;
+		q = temp;
+	}
+	cout << *p << *q << endl;
 }
 
 
 
 int main() {
+	double x;
+	cin >> x;
+	
+	double y;
+	cin >> y;
+
+	//cout << x << y << endl;
+	sort2(&x, &y);
+	//cout << x << y << endl;
 
 	return 0;
 }
